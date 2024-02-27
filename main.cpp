03781d601373f8893e764b260ac115ba9492d8c1
isym444 <samir.yep@gmail.com>
2024-02-27 23:33:00 +0000

    Updated template

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@
             "#include <deque>",
             "#include <numeric>",
             "#include <assert.h>",
+            "#include <unordered_map>",
         ])
     if not shutil.which("clang-format"):
         logger.warning("clang-format is not installed. If you want to generate well-formatted code, please install it. If you use Ubuntu, you can run $ sudo apt install clang-format")
@@ -171,6 +172,96 @@ void gen_primes() {
             prime[x] = 0;
         }
     }
+}
+
+const int MAXN = 2 * 100000 + 10; // Adjust the size as per the problem constraints
+//smallest prime factor
+std::vector<int> spf(MAXN);
+
+//Sieve of Eratosthenes to precompute the smallest prime factor of each number
+void spfsieve() {
+    //initializes smallest prime factor for 1 as 1
+    spf[1] = 1;
+    //initially treats every number as a prime and assigns the smallest prime factor as itself
+    for (int i = 2; i < MAXN; ++i) {
+        spf[i] = i;
+    }
+    //all even numbers' smallest prime factor is 2. This sets this for all even numbers
+    for (int i = 4; i < MAXN; i += 2) {
+        spf[i] = 2;
+    }
+    //calculates upper limit to which we need to check for primes (optimization)
+    //because factors come in pairs e.g. N = 36, its factors are (1, 36), (2, 18), (3, 12), (4, 9), and (6, 6)
+    //so after sqrt(N) the pairs will switch and and won't find any further new factors
+    //Factors of a number come in pairs, with one factor being less than or equal to the square root of the number,
+    //and the other being greater than or equal to the square root.
+    int limit = std::ceil(std::sqrt(MAXN));
+    for (int i = 3; i < limit; i+=2) {
+        //checks if i is still marked as it's own smallest prime factor
+        //indicating that it is still prime 
+        //because we have iterated through all smaller numbers beforehand so if not a multiple of any of those
+        //then by definition it must be prime
+        if (spf[i] == i) {
+            //start from i*i as spf for smaller numbers than this would be marked by smaller numbers than i (if not prime)
+            for (int j = i * i; j < MAXN; j += i) {
+                //if smallest prime factor of j is still set as itself i.e. spf not yet been set
+                //then set spf as i
+                if (spf[j] == j) {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+}
+
+//Function to return the prime factorization of a number (is unique for every number)
+//make sure to call spfsieve() before calling this function so spf values are prepopulated
+std::unordered_map<int, int> fact(int x) {
+    std::unordered_map<int, int> pfactors;
+    while (x != 1) {
+        //if the smallest prime factor of x has not yet been added to pfactors
+        //  then set p^1
+        //if the smallest prime factor of x has already been added to pfactors
+        //  then set p^(prev exponent+1)
+        if (pfactors.find(spf[x]) == pfactors.end()) {
+            pfactors[spf[x]] = 1;
+        } else {
+            pfactors[spf[x]] += 1;
+        }
+        //while x!=1, divide x by its smallest prime factor
+        x = x / spf[x];
+    }
+    return pfactors;
+}
+
+//use if possible as faster than nCx
+long long nC2(int n) {
+    return static_cast<long long>(n) * (n - 1) / 2;
+}
+
+//order matters so larger than nCx
+long long nPx(int n, int x) {
+    if (x > n) return 0;
+    
+    long long result = 1;
+    for (int i = n; i > n - x; --i) {
+        result *= i;
+    }
+    return result;
+}
+
+//order doesn't matter so smaller than nPx
+long long nCx(int n, int x) {
+    if (x > n) return 0;
+    if (x * 2 > n) x = n - x; // Take advantage of symmetry, nCx == nC(n-x)
+    if (x == 0) return 1;
+
+    long long result = 1;
+    for (int i = 1; i <= x; ++i) {
+        result *= n - (x - i);
+        result /= i;
+    }
+    return result;
 }
 
 vector<ll> parent; // To store parent information
@@ -555,6 +646,18 @@ struct loc
     inline static char dir='r';
     //loc::x to access or modify x
 };
+
+vector<ll> genAlphabetPlaceholder(){
+    vector<ll> f(26);
+    iota(f.begin(), f.end(), 0);
+    return f;
+}
+
+vector<char> genAlphabet(){
+    vector<char> alphabet(26);
+    iota(alphabet.begin(), alphabet.end(), 'a');
+    return alphabet;
+}
 
 //Graph visualizer:
 //https://csacademy.com/app/graph_editor/
