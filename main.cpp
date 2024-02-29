4db7d2bf570ddd2acc219b0c620d7b4adecc1290
isym444 <samir.yep@gmail.com>
2024-02-29 10:50:27 +0000

    Updated template

--- main.cpp
+++ main.cpp
@@ -236,6 +236,23 @@ std::unordered_map<int, int> fact(int x) {
     return pfactors;
 }
 
+ll mod=1e9+7
+//ll mod=1000;
+//modular exponentiation: calculates a^b mod c where a^b is a crazy big number and would usually overflow. Change mod above as needed
+ll mpow(ll base, ll exp)
+{
+    base %= mod;
+    ll result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
 //use if possible as faster than nCx
 long long nC2(int n) {
     return static_cast<long long>(n) * (n - 1) / 2;
