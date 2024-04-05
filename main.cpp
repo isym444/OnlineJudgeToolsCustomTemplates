1ffa8d937b9cba5cef4fd308c77a22b95c5a0fc6
isym444 <samir.yep@gmail.com>
2024-04-05 01:55:48 +0100

    Updated template

--- main.cpp
+++ main.cpp
@@ -612,6 +612,97 @@ void dfsSubtrees(ll startNode){
     }
 }
 
+// lowest common ancestor (LCA) class for undirected weighted tree
+struct UndirectedWeightedTree {
+    using T = long long; // Arbitrary data structure (operator+, operator- must be defined)
+    int INVALID = -1;
+    int V, lgV;
+    int E;
+    int root;
+    std::vector<std::vector<std::pair<int, int>>> adj; // (nxt_vertex, edge_id)
+    // vector<pint> edge; // edges[edge_id] = (vertex_id, vertex_id)
+    std::vector<T> weight;     // w[edge_id]
+    std::vector<int> par;      // parent_vertex_id[vertex_id]
+    std::vector<int> depth;    // depth_from_root[vertex_id]
+    std::vector<T> acc_weight; // w_sum_from_root[vertex_id]
+
+    void _fix_root_dfs(int now, int prv, int prv_edge_id) {
+        par[now] = prv;
+        if (prv_edge_id != INVALID) acc_weight[now] = acc_weight[prv] + weight[prv_edge_id];
+        for (auto nxt : adj[now])
+            if (nxt.first != prv) {
+                depth[nxt.first] = depth[now] + 1;
+                _fix_root_dfs(nxt.first, now, nxt.second);
+            }
+    }
+
+    UndirectedWeightedTree() = default;
+    UndirectedWeightedTree(int N) : V(N), E(0), adj(N) {
+        lgV = 1;
+        while (1 << lgV < V) lgV++;
+    }
+
+    void add_edge(int u, int v, T w) {
+        adj[u].emplace_back(v, E);
+        adj[v].emplace_back(u, E);
+        // edge.emplace_back(u, v);
+        weight.emplace_back(w);
+        E++;
+    }
+
+    //Have to set which node you want to consider as root. This function will then run dfs to calculate depth for every node
+    void fix_root(int r) {
+        root = r;
+        par.resize(V);
+        depth.resize(V);
+        depth[r] = 0;
+        acc_weight.resize(V);
+        acc_weight[r] = 0;
+        _fix_root_dfs(root, INVALID, INVALID);
+    }
+
+    std::vector<std::vector<int>> doubling;
+    void doubling_precalc() {
+        doubling.assign(lgV, std::vector<int>(V));
+        doubling[0] = par;
+        for (int d = 0; d < lgV - 1; d++)
+            for (int i = 0; i < V; i++) {
+                if (doubling[d][i] == INVALID)
+                    doubling[d + 1][i] = INVALID;
+                else
+                    doubling[d + 1][i] = doubling[d][doubling[d][i]];
+            }
+    }
+
+    int kth_parent(int x, int k) {
+        if (depth[x] < k) return INVALID;
+        for (int d = 0; d < lgV; d++) {
+            if (x == INVALID) return INVALID;
+            if (k & (1 << d)) x = doubling[d][x];
+        }
+        return x;
+    }
+
+    //returns the lca of 2 nodes
+    int lowest_common_ancestor(int u, int v) {
+        if (depth[u] > depth[v]) std::swap(u, v);
+
+        v = kth_parent(v, depth[v] - depth[u]);
+        if (u == v) return u;
+        for (int d = lgV - 1; d >= 0; d--) {
+            if (doubling[d][u] != doubling[d][v]) u = doubling[d][u], v = doubling[d][v];
+        }
+        return par[u];
+    }
+
+    //uses LCA to calculate distance between 2 nodes in O(log N) time
+    T path_length(int u, int v) {
+        // Not distance, but the sum of weights
+        int r = lowest_common_ancestor(u, v);
+        return (acc_weight[u] - acc_weight[r]) + (acc_weight[v] - acc_weight[r]);
+    }
+};
+
 vector<vector<ll> >ddist;
 
 void floydWarshall(ll n) {
