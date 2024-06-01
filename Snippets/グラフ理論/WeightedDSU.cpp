0ea9f4331e3a3ef9ad9a6c8c4b9ff0f826f679ed
isym444 <samir.yep@gmail.com>
2024-06-01 17:45:45 +0100

    Updated template

--- Snippets/グラフ理論/WeightedDSU.cpp
+++ Snippets/グラフ理論/WeightedDSU.cpp
@@ -1,10 +1,13 @@
 // Weighted UnionFind
 template <class S> struct WeightedUnionFind {
-    std::vector<int> par, sz;
+    //par for parent i.e. representative node of set (initially itself), size of set a node belongs to (only if node the root of that set) but n.b. count() returns appropriately
+    std::vector<int> par, size;
+    //pot stores the potential/weight of each element relative to the root of its set i.e. the distance to the root of the set for each node (given the weights of the edges)
     std::vector<S> pot;
-    WeightedUnionFind(int N = 0) : par(N), sz(N, 1), pot(N) {
+    WeightedUnionFind(int N = 0) : par(N), size(N, 1), pot(N) {
         std::iota(par.begin(), par.end(), 0);
     }
+    //finds the root of the set containing x
     int find(int x) {
         if (par[x] != x) {
             int r = find(par[x]);
@@ -12,17 +15,23 @@ template <class S> struct WeightedUnionFind {
         }
         return par[x];
     }
+    //unites sets containing s & t with a relative difference rel_diff and checks for contradictions
     bool unite(int s, int t, S rel_diff) {
         // Relate s and t by f[t] = f[s] + rel_diff
-        // Return false iff contradiction happens.
+        // Return false if contradiction happens.
         rel_diff = rel_diff + weight(s) + (-weight(t));
+        // If already in the same set, check for a contradiction in pre-existing weights and if so, return false else return true
         if ((s = find(s)) == (t = find(t))) return rel_diff == 0;
-        if (sz[s] < sz[t]) std::swap(s, t), rel_diff = -rel_diff;
-        par[t] = s, sz[s] += sz[t], pot[t] = rel_diff;
+        if (size[s] < size[t]) std::swap(s, t), rel_diff = -rel_diff;
+        par[t] = s, size[s] += size[t], pot[t] = rel_diff;
         return true;
     }
+    //returns weight of x relative to root of its set
     S weight(int x) { return find(x), pot[x]; }
+    //returns difference in potential between t and s N.B. that ORDER MATTERS e.g. if want coordinates, put the node at 0,0 as first argument i.e. s
     S diff(int s, int t) { return weight(t) + (-weight(s)); } // return f[t] - f[s]
-    int count(int x) { return sz[find(x)]; }
+    //returns size of set containing x
+    int count(int x) { return size[find(x)]; }
+    //checks if s and t are in same set
     bool same(int s, int t) { return find(s) == find(t); }
 };
