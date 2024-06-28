614a2a4a5ef66d07a36171f5e936870a46bbf2b7
isym444 <samir.yep@gmail.com>
2024-06-28 23:33:00 +0100

    Updated template

--- Snippets/Lambda.cpp
+++
@@ -1,7 +0,0 @@
-auto find = [&](auto&& self, int x) -> int {
-    if (parent[x] == x) {
-        return x;
-    } else {
-        return parent[x] = self(self, parent[x]);
-    }
-};
---
+++ Snippets/RecursiveLambda.cpp
@@ -0,0 +1,24 @@
+auto dfs = [&](auto dfs, ll v) -> void {
+    if(ans==1e6) return;
+    visited[v]=1;
+    ans++;
+    for(auto x:adj[v]){
+        if(!visited[x]){
+            dfs(dfs, x);
+            // ans++;
+        }
+    }
+    visited[v]=0;
+};
+
+dfs(dfs,(ll)0);
+
+
+
+auto find = [&](auto&& self, int x) -> int {
+    if (parent[x] == x) {
+        return x;
+    } else {
+        return parent[x] = self(self, parent[x]);
+    }
+};
