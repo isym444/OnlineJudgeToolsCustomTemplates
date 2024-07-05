05e6a2c9e1fbb62d72e8bf9f88ec12a545190bb8
isym444 <samir.yep@gmail.com>
2024-07-05 13:25:03 +0100

    Updated template

--- Snippets/グラフ理論/TopologicalSort.cpp
+++ Snippets/グラフ理論/TopologicalSort.cpp
@@ -1,12 +1,23 @@
 vll ans;
-vvll P;
+vvll to;
 vll visited;
 
 void dfs(ll node){
     visited[node]=1;
-    for(auto neighbour:P[node]){
+    for(auto neighbour:to[node]){
         if(visited[neighbour]) continue;
         dfs(neighbour);
     }
     ans.pb(node);
+}
+
+//check there is only one valid topological order
+foi(0,N-1) {
+    int a = ans[i], b = ans[i+1];
+    bool ok = false;
+    for (int v : to[a]) if (v == b) ok = true;
+    if (!ok) { 
+        cout << "No" << endl; 
+        return 0; 
+    }
 }
