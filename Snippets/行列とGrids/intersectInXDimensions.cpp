dbabc8a4f79b7da757adbce48b77b0d1c3a5bc6d
isym444 <samir.yep@gmail.com>
2024-07-07 23:33:00 +0100

    Updated template

--- Snippets/行列とGrids/intersectInXDimensions.cpp
+++ Snippets/行列とGrids/intersectInXDimensions.cpp
@@ -1,5 +1,5 @@
 //https://atcoder.jp/contests/abc361/editorial/10353
-//check x, y and z lines intersect
+//use this to check x, y and z lines intersect as per needed i.e. if 2d or 3d problem
 auto intersect = [&](ll l1, ll r1, ll l2, ll r2){
         return max(l1, l2) < min(r1,r2);
     };
