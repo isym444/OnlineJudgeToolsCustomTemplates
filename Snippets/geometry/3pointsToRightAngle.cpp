99b254aa826f91de93032c5c4419059012708158
isym444 <samir.yep@gmail.com>
2024-07-15 23:33:00 +0100

    Updated template

---
+++ Snippets/geometry/3pointsToRightAngle.cpp
@@ -0,0 +1,12 @@
+ll ax, ay, bx, by, cx, cy;
+cin >> ax >> ay >> bx >> by >> cx >> cy;
+ll AB2 = (bx-ax)*(bx-ax)+(by-ay)*(by-ay);
+ll BC2 = (cx-bx)*(cx-bx)+(cy-by)*(cy-by);
+ll CA2 = (cx-ax)*(cx-ax)+(cy-ay)*(cy-ay);
+
+if(AB2+BC2==CA2||AB2+CA2==BC2||BC2+CA2==AB2){
+    cout << "Yes" << endl;
+    return 0;
+}
+
+cout << "No" << endl;
