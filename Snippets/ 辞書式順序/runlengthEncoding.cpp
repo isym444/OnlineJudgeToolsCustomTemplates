04430f651e711fd54968ebdb12037cca020e2aad
Ian Samir Yep Manzano <samir.yep@gmail.com>
2024-11-21 16:16:19 +0000

    update

---
+++ Snippets/ 辞書式順序/runlengthEncoding.cpp
@@ -0,0 +1,18 @@
+vector<pair<char, int>> runLengthEncoding(string s) {
+	int n = s.length();
+
+	vector<pair<char, int>> res;
+	char pre = s[0];
+	int cnt = 1;
+	rep(i, 1, n) {
+		if (pre != s[i]) {
+			res.push_back({ pre, cnt });
+			pre = s[i];
+			cnt = 1;
+		}
+		else cnt++;
+	}
+
+	res.push_back({ pre, cnt });
+	return res;
+}
