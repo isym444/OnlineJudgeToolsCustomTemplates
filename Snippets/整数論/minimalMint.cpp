1f13a35be73dcf78bdc1847944081bc08ff6cbb8
Ian Samir Yep Manzano <samir.yep@gmail.com>
2024-11-29 12:17:02 +0000

    update

--- Snippets/整数論/minimalMint.cpp
+++ Snippets/整数論/minimalMint.cpp
@@ -1,3 +1,51 @@
+
+const int mod = 1000000007;
+struct mint {
+    // using ll = long long; // Ensure 'll' is defined within the struct
+    ll x;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    // For prime mod
+    mint inv() const { return pow(mod - 2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+
+    // Define operator== and operator!=
+    bool operator==(const mint& a) const { return x == a.x; }
+    bool operator!=(const mint& a) const { return x != a.x; }
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+
+
+
+
+
+
 // https://atcoder.jp/contests/abc163/submissions/12199470
 // auto mod int
 // https://youtu.be/L8grWxBlIZ4?t=9858
