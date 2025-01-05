6ab5129e8e52f262f2fd63ccd91ebe767dd14879
isym444 <samir.yep@gmail.com>
2025-01-05 22:58:24 +0000

    c

--- Snippets/行列とGrids/isvalid.cpp
+++ Snippets/行列とGrids/isvalid.cpp
@@ -3,4 +3,6 @@
 #define isvalid(x_value,y_value,min_valid_x,max_valid_x,min_valid_y,max_valid_y) (min_valid_x<=x_value and x_value<max_valid_x and min_valid_y<=y_value and y_value<max_valid_y)
 #define isvalidsingle(value,min_valid_value,max_valid_value) (min_valid_value<=value and value<max_valid_value)
 
+#define iv(value,min_valid_value,max_valid_value) (min_valid_value<=value and value<max_valid_value)
+
 //#define gcd __gcd
