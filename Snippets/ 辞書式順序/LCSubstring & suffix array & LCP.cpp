02c3e9adb3b8db57183cb17bf3ea950433dc1a8c
isym444 <samir.yep@gmail.com>
2025-03-26 12:42:24 +0000

    added extra example for use of suffix array

--- Snippets/ 辞書式順序/LCSubstring & suffix array & LCP.cpp
+++ Snippets/ 辞書式順序/LCSubstring & suffix array & LCP.cpp
@@ -1,11 +1,42 @@
 
+// suffix array is the array of starting indices of the sorted suffixes of a string
+// e.g. banana suffixes:
+/* 
+"banana"
+"anana"
+"nana"
+"ana"
+"na"
+"a"
+*/
+// when sorted:
+/* 
+"banana"
+"anana"
+"nana"
+"ana"
+"na"
+"a"
+*/
+// so suffix array is: SA = [5, 3, 1, 0, 4, 2]
 
-// longest repeated substring within a single string
+// An LCP Array tells us the length of the longest common prefix between each pair of consecutive suffixes in the sorted suffix array.
+// for banana suffix array: LCP = [0, 1, 3, 0, 0, 2]
+
+
+//  Given a string 'S' and a string 'T' determine whether the string T is a substring of S, if so return the index at which T is a substring of S.
+/* Solution using Suffix Array: We can notice that any substring is a prefix of some suffix. In the suffix array for string 'S' we cut off the first |T| characters of each suffix and get all the substring of length atmost |T| in a sorted order. In order to find S we can simply apply binary search and compare the mid string to string S.
+
+If mid string of suffix array is lexicographically smaller than 'T' then binary search on right half.
+If mid string of suffix array is lexicographically greater than 'T' then binary search on left half.
+If both the string match return that index as our result. */
+
+
+// longest repeated substring within a single string (longest common prefix for the suffixes starting at i & j)
 cin >> S;
 auto sa = suffix_array(S);
 auto lcp = lcp_array(S,sa);
 // largest value in lcp is the length of the longest repeated substring
-
 
 // Count of distinct substrings in a single string:
 
@@ -15,12 +46,14 @@ dbg(sa);
 auto lcp = lcp_array(S, sa);
 dbg(lcp);
 ll n = S.length();
+// total substrings
 ll ans = n*(n+1)/2;
 dbg(ans);
 ll sum=0;
 fx(lcp){
     sum+=x;
 }
+// remove duplicate substrings
 ans-=sum;
 cout << ans << endl;
 
